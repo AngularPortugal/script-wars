893d25058bc6c034e083bb3aad8b21b99e7e834c
Vitor Fernandes <vmlf01@gmail.com>
2016-02-18 21:33:09 +0000

    STEP 10b: Reaching out to the world

--- public/js/films.controller.js
+++ public/js/films.controller.js
@@ -4,13 +4,17 @@
     angular.module('scriptWarsApp')
         .controller('FilmsController', FilmsController);
 
-    FilmsController.$inject = ['$state', '$stateParams', 'FilmsService'];
-    function FilmsController($state, $stateParams, FilmsService) {
+    FilmsController.$inject = ['$state', '$stateParams', 'FilmsService', '_'];
+    function FilmsController($state, $stateParams, FilmsService, _) {
         var vm = this;
 
         vm.name = $stateParams.name;
         vm.selectedSide = $stateParams.side;
-        vm.films = FilmsService.getFilms();
+
+        FilmsService.getFilms()
+            .then(function (films) {
+                vm.films = _.sortBy(films, 'releaseYear');
+            });
 
         if (vm.name === '' || vm.selectedSide === '') {
             $state.go('home');
